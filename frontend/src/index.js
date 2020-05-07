248ef20e7a1d525614a5efc7fb667ddb534b8dc9
howardbdev <howarddevennish@gmail.com>
2020-05-06 21:05:50 -0400

    grab redux, thunk, and router, also set up store and devtools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore((s, a)=>({name: "Howard"}), composeEnhancers(
+    applyMiddleware(thunk)
+  ));
+
 
 ReactDOM.render(
   <React.StrictMode>
