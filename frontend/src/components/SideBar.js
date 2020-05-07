3adfbe41885e0681c982b1d884e749fb47aa21ae
howardbdev <howarddevennish@gmail.com>
2020-05-06 21:43:26 -0400

    remove now redundant button

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const SideBar = ({cars, handleCarLinkClick, newCarClick}) => (
   <div className="SideBar">
-    <button className="main-link" onClick={()=>newCarClick("newCar")}>Dealer: Add New Car</button>
     <h4>Cars Inventory </h4>
     {cars.map(car => <a key={car.id} onClick={() => handleCarLinkClick(car.id)}>{car.year} {car.make} {car.model}</a>)}
   </div>
