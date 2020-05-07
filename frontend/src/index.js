890e35e3061fb65deacc0c78e908a1f6b43e2147
howardbdev <howarddevennish@gmail.com>
2020-05-06 21:13:05 -0400

    initial store setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,17 +6,22 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import rootReducer from "./reducers/rootReducer.js"
 import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore((s, a)=>({name: "Howard"}), composeEnhancers(
+const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
   ));
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
